5afeead1b0eb873ae8d10fdcf10a2e4fbc112315
Wupxy <hamsuf24@gmail.com>
2023-03-11 22:26:11 +0700

    fixing

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -106,7 +106,7 @@ const CreatePost = () => {
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          <div className="relative bg-gray-50 dark:bg-gray-900 border border-gray-300 dark:border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             { form.photo ? (
               <img
                 src={form.photo}
